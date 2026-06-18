04cd817e9a08d97dd9a899cf7adf50a6340bd24f
Abdullah Sultan <abdullahmsultan1@gmail.com>
2026-06-18 11:11:52 -0500

    chore: ignore .next build cache

--- apps/web/.next/static/chunks/pages/_app-a5b5153b52bdf9c5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{7288:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(933)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[6593,8792],()=>(n(7288),n(3881))),_N_E=_.O()}]);
